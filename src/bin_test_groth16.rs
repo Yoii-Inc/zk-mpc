8666d3b5605b51ce5b17db218221f74fb05e4bfd
sheagrief <3a.mad1earth4@gmail.com>
2023-10-21 05:52:16 +0900

    :construction: Implement reveal & script for Groth16

---
+++ src/bin_test_groth16.rs
@@ -0,0 +1,23 @@
+use mpc_net::{MpcMultiNet as Net, MpcNet};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+mod circuit;
+mod groth16;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+struct Opt {
+    /// Id
+    id: usize,
+
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    Net::init_from_file(opt.input.to_str().unwrap(), opt.id);
+    // groth16::mpc_test_prove_and_verify(1);
+}
