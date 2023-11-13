5b82d449c131b44647e23ccae99c998cba675404
sheagrief <3a.mad1earth4@gmail.com>
2023-11-14 02:24:07 +0900

    :sparkles: Implement werewolf script

---
+++ run_werewolf.zsh
@@ -0,0 +1,66 @@
+set -ex
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
+cargo build --bin bin-werewolf
+BIN=./target/debug/bin-werewolf
+
+PROCS=()
+
+mode=$1
+players=3
+
+case $mode in
+init)
+    for i in $(seq 0 $((players - 1))); do
+        mkdir -p ./werewolf/$i
+    done
+    RUST_BACKTRACE=1 $BIN init --num-players $players
+    wait
+    for i in $(seq 0 $((players - 1))); do
+        mkdir -p ./werewolf/$i
+        if [ $i == 0 ]; then
+            RUST_BACKTRACE=1 $BIN preprocessing $i ./data/address &
+            pid=$!
+            PROCS[$i]=$pid
+        else
+            $BIN preprocessing $i ./data/address >/dev/null &
+            pid=$!
+            PROCS[$i]=$pid
+        fi
+    done
+
+    for pid in ${PROCS[@]}; do
+        wait $pid
+    done
+    ;;
+night)
+    if [ ! -f "./werewolf/public.json" ]; then
+        echo "not found public.json"
+        exit 1
+    fi
+    for i in $(seq 0 $((players - 1))); do
+        if [ $i == 0 ]; then
+            RUST_BACKTRACE=1 $BIN night --target 1 $i ./data/address &
+            pid=$!
+            PROCS[$i]=$pid
+        else
+            $BIN night $i ./data/address >/dev/null &
+            pid=$!
+            PROCS[$i]=$pid
+        fi
+    done
+
+    for pid in ${PROCS[@]}; do
+        wait $pid
+    done
+    ;;
+*)
+    echo "invalid mode: $mode"
+
+    echo "Usage: $0 [init|mode]"
+    exit 1
+    ;;
+esac
+
+echo done
