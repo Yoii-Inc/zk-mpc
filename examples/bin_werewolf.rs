d42e34cea9aa171624e43bf06f9df84c2ca73e66
sheagrief <3a.mad1earth4@gmail.com>
2024-09-17 15:44:45 +0900

    ✨ Implement shuffle matrix and tests.

--- examples/bin_werewolf.rs
+++ examples/bin_werewolf.rs
@@ -14,6 +14,8 @@ use mpc_algebra::encryption::elgamal::elgamal::Parameters;
 use mpc_algebra::malicious_majority::*;
 use mpc_algebra::Reveal;
 use mpc_net::{MpcMultiNet as Net, MpcNet};
+use rand::seq::SliceRandom;
+use rand::Rng;
 use serde::Deserialize;
 use std::collections::BTreeMap;
 use std::collections::HashMap;
@@ -295,6 +297,120 @@ fn preprocessing_werewolf(opt: &Opt) -> Result<(), std::io::Error> {
     Ok(())
 }
 
+// Compute shuffle matrix and return role and player id in the same group.
+fn calc_shuffle_matrix(
+    grouping_parameter: &GroupingParameter,
+    shuffle_matrix: &[na::DMatrix<MFr>],
+    id: usize,
+) -> Result<(Roles, Option<Vec<usize>>), std::io::Error> {
+    // parameters
+    let n = grouping_parameter.get_num_players();
+    let m = grouping_parameter.get_num_groups();
+
+    // generate tau matrix
+    let tau_matrix = grouping_parameter.generate_tau_matrix();
+
+    // compute rho matrix
+    let m_matrix = shuffle_matrix
+        .iter()
+        .fold(na::DMatrix::<MFr>::identity(n + m, n + m), |acc, x| acc * x);
+    let rho_matrix = m_matrix.transpose() * tau_matrix * m_matrix;
+
+    // iterate. get rho^1, rho^2, ..., rho^num_players
+    let mut rho_sequence = Vec::with_capacity(n);
+    let mut current_rho = rho_matrix.clone();
+    for _ in 0..n {
+        rho_sequence.push(current_rho.clone());
+        current_rho *= rho_matrix.clone(); // rho^(i+1) = rho^i * rho
+    }
+
+    let mut unit_vec = na::DVector::<MFr>::zeros(n + m);
+    unit_vec[id] = MFr::one();
+
+    // player i: for each j in {1..n}, get rho^j(i)
+    let result = rho_sequence
+        .iter()
+        .map(|rho| rho * unit_vec.clone())
+        .map(|x| {
+            let index = x.column(0).into_iter().enumerate().find_map(|(j, value)| {
+                if *value != MFr::zero() {
+                    Some(j)
+                } else {
+                    None
+                }
+            });
+            index.unwrap_or_else(|| panic!("Error: No index found"))
+        }) // search for the index of the one element
+        .collect::<Vec<_>>();
+
+    println!("player {:?} result is {:?}", id, result);
+
+    // get role value. get val which is max value in result.
+    let role_val = result.iter().max().expect("Failed to get max value");
+
+    // get role
+    let role = grouping_parameter.get_corresponding_role(*role_val);
+
+    let mut fellow = result
+        .iter()
+        .filter(|x| **x != id && **x < n)
+        .copied()
+        .collect::<Vec<_>>();
+
+    if fellow.is_empty() {
+        Ok((role, None))
+    } else {
+        fellow.sort();
+        fellow.dedup();
+        Ok((role, Some(fellow)))
+    }
+}
+
+fn generate_individual_shuffle_matrix<R: Rng>(n: usize, m: usize, rng: &mut R) -> na::DMatrix<MFr> {
+    let mut shuffle_matrix = na::DMatrix::<MFr>::zeros(n + m, n + m);
+
+    // generate permutation
+    let mut permutation: Vec<usize> = (0..n).collect();
+    permutation.shuffle(rng);
+
+    // shuffle_matrix
+    for i in 0..n {
+        shuffle_matrix[(i, permutation[i])] = MFr::one();
+    }
+
+    for i in n..n + m {
+        shuffle_matrix[(i, i)] = MFr::one();
+    }
+
+    shuffle_matrix
+}
+
+#[test]
+fn test_shuffle_matrix() {
+    let grouping_parameter = GroupingParameter::new(
+        vec![
+            (Roles::Villager, (4, false)),
+            (Roles::FortuneTeller, (1, false)),
+            (Roles::Werewolf, (2, true)),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
+    let shuffle_matrix = vec![generate_individual_shuffle_matrix(
+        grouping_parameter.get_num_players(),
+        grouping_parameter.get_num_groups(),
+        &mut test_rng(),
+    )];
+
+    for id in 0..grouping_parameter.get_num_players() {
+        let (role, player_ids) =
+            calc_shuffle_matrix(&grouping_parameter, &shuffle_matrix, id).unwrap();
+        println!("role is {:?}", role);
+        println!("fellow is {:?}", player_ids);
+    }
+}
+
 struct GroupingParameter(BTreeMap<Roles, (usize, bool)>);
 
 impl GroupingParameter {
@@ -358,6 +474,18 @@ impl GroupingParameter {
     fn get_num_players(&self) -> usize {
         self.0.values().map(|x| x.0).sum()
     }
+
+    fn get_corresponding_role(&self, role_id: usize) -> Roles {
+        let mut count = self.get_num_players();
+        for (role, (role_count, is_not_alone)) in self.0.iter() {
+            count += if *is_not_alone { 1 } else { *role_count };
+            if role_id < count {
+                return role.clone();
+            }
+        }
+
+        panic!("Error: Invalid role id is given");
+    }
 }
 
 #[test]
@@ -397,7 +525,7 @@ fn night_werewolf(opt: &Opt) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
+#[derive(Debug, PartialEq, Eq, Hash, Ord, PartialOrd, Clone)]
 enum Roles {
     FortuneTeller,
     Werewolf,
